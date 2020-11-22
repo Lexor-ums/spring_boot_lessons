9e4b5766fdaa17e1e3ba9be2dae0beb49d7cba14
Lexor <su.lexxor@gmail.com>
2020-11-22 23:02:39 +0400

    lesson3: securuty with roles

---
+++ src/main/java/com/example/spring_lesson/configuration/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.spring_lesson.configuration;
+
+import com.example.spring_lesson.model.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "today_weather/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "weather_log/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1"))
+                .roles(Role.ADMIN.name())
+                .build(),
+
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("2"))
+                        .roles(Role.USER.name())
+                        .build()
+        );
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+}
---
+++ src/main/java/com/example/spring_lesson/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.spring_lesson.model;
+
+public enum Role {
+    ADMIN, USER
+}
